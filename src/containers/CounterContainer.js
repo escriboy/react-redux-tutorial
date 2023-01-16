49b61e05cd00d7da6aef4938d544b4d34a63734c
YM CHOI <escriboy@gmail.com>
2023-01-16 15:30:42 +0900

    counter, todos

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Counter from '../components/Counter';
+import { connect } from 'react-redux';
+import { decrease, increase } from '../modules/counter';
+import { bindActionCreators } from 'redux';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  },
+)(CounterContainer);
