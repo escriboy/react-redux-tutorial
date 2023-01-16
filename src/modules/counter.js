4661625bc8bdd655f6fc0c96741e8d190dec9765
YM CHOI <escriboy@gmail.com>
2023-01-16 14:51:09 +0900

    first commit

---
+++ src/modules/counter.js
@@ -0,0 +1,26 @@
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// 초기 상태
+const initialState = {
+  number: 0,
+};
+
+// 리듀서 함수
+export default function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+}
